c7a143623f957a12f18c75af1d4bd6ff22abc568
KJ-M <rfb0920@gmail.com>
2024-06-14 20:54:28 +0900

    問題を作成し、１つ目の選択肢に正誤判定をつけました。

--- app.js
+++ app.js
@@ -7,4 +7,19 @@ const answers = [
 ];
 const correct = 'ニンテンドーDS';
 
-console.log(document.getElementById('js-question'));
+console.log();
+document.getElementById('js-question').textContent = question
+
+const $button = document.getElementsByTagName('button')
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
+
+document.getElementsByTagName('button')[0].addEventListener('click', () => {
+    if(correct === $button){
+        window.alert('正解！');
+    } else {
+        window.alert('不正解！');
+    }
+});
