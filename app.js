98d2d5ba799a56965b94412e5468053e601c7a8c
KJ-M <rfb0920@gmail.com>
2024-06-14 18:20:00 +0900

    app.js, index.html, style.cssを作成して、app.jsにクイズを一つ作成しました。

---
+++ app.js
@@ -0,0 +1,10 @@
+const question = 'ゲーム市場、最も売れたゲーム機は次の内どれ？'
+const answers = [
+    'スーパーファミコン',
+    'プレイステーション２',
+    'ニンテンドースイッチ',
+    'ニンテンドーDS'
+];
+const correct = 'ニンテンドーDS';
+
+console.log(document.getElementById('js-question'));
